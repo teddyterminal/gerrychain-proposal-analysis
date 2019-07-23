22fb7ceb63e3896131c3b1318c6473d197daacba
Rishabh Chandra <rishabh@Rishabhs-MacBook-Pro.local>
2019-07-23 05:55:27 -0400

    added debugs

--- code/node_neuralnet.py
+++ code/node_neuralnet.py
@@ -20,6 +20,7 @@ model.compile(loss = 'binary_crossentropy',
 	optimizer = sgd, 
 	metrics = ['accuracy'])
 
+print("Reading Data Files..... ")
 neutral = pd.read_csv("../data/generated_datasets/PA_BN_50000_20190720")
 gop = pd.read_csv("../data/generated_datasets/PA_GOPBN_50000_20190720")
 del neutral["Unnamed: 0"]
@@ -28,7 +29,10 @@ del gop["Unnamed: 0"]
 neutral["Y"] = 0
 gop["Y"] = 1
 
+print("Combining Data......")
 combined = pd.concat([neutral, gop])
+
+print("Shuffling and sampling data.....")
 combined = combined.sample(frac=1).reset_index(drop=True)
 
 Y = combined["Y"].values
@@ -41,6 +45,7 @@ Ytrain = Y[:81600]
 Xtest = X[81600:]
 Ytest = Y[81600:]
 
+print("Training.....")
 history = model.fit(x = Xtrain, y = Ytrain, batch_size = 128, epochs = 20, 
 	verbose = 2, validation_split = 0.125, shuffle = True, validation_freq=1)
 
